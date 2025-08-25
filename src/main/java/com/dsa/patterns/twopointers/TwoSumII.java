eed9a868fadab4beecdefe324ee2039d8b4167c4
aishwary-03 <aishwary987654321@gmail.com>
2025-08-25 22:59:31 +0530

    Two Sum II

--- src/main/java/com/dsa/patterns/twopointers/TwoSumII.java
+++ src/main/java/com/dsa/patterns/twopointers/TwoSumII.java
@@ -1,4 +1,28 @@
 package com.dsa.patterns.twopointers;
 
+import java.util.Arrays;
+
 public class TwoSumII {
+
+    public static void main(String[] args) {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] indexes = twoSumII.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(indexes));
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            }
+        }
+        return null;
+    }
+
 }
