bb243b38e5c3191d3186eb8935c60464575f3ffa
aishwary-03 <aishwary987654321@gmail.com>
2026-05-09 18:15:45 +0530

    Three Sum

--- src/main/java/com/dsa/patterns/twopointers/ThreeSum.java
+++ src/main/java/com/dsa/patterns/twopointers/ThreeSum.java
@@ -8,35 +8,43 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         ThreeSum threeSum = new ThreeSum();
-        List<List<Integer>> triplets = threeSum.threeSum(new int[]{0, 1, 1});
-        System.out.println(triplets);
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum.threeSum(nums));
+
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    private List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
+
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) return triplets;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.length - 1;
+
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-
                 if (sum == 0) {
-                    triplets.add(List.of(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    List<Integer> triplet = List.of(nums[i], nums[left], nums[right]);
+                    triplets.add(triplet);
+                    left++;
+                    right--;
 
+                    while (left < right && nums[left - 1] == nums[left]) left++;
+                    while (left < right && nums[right + 1] == nums[right]) right--;
+                } else if (sum < 0) {
                     left++;
-                    right--;
-                } else if (sum > 0) {
-                    right--;
                 } else {
-                    left++;
+                    right--;
                 }
             }
+
         }
+
         return triplets;
     }
+
+
 }
