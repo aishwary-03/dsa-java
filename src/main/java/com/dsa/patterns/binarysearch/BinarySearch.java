83924b9112d9ed9e4f3ce9edc1176196793ce756
aishwary-03 <aishwary987654321@gmail.com>
2026-05-11 23:09:47 +0530

    Binary Search

--- src/main/java/com/dsa/patterns/binarysearch/BinarySearch.java
+++ src/main/java/com/dsa/patterns/binarysearch/BinarySearch.java
@@ -4,23 +4,27 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
-        int index = binarySearch.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
-        System.out.println(index);
+        int[] nums = new int[] {5};
+        System.out.println(binarySearch.search(nums, 5));
     }
 
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1;
+                left= mid + 1;
             } else {
                 right = mid - 1;
             }
+
         }
+
         return -1;
     }
+
 }
