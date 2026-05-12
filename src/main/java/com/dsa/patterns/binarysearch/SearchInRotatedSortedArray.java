b9eff43aaf0473fb909aad51e3e533a27874041a
aishwary-03 <aishwary987654321@gmail.com>
2026-05-12 20:32:28 +0530

    Search In Rotated Sorted Array

--- src/main/java/com/dsa/patterns/binarysearch/SearchInRotatedSortedArray.java
+++ src/main/java/com/dsa/patterns/binarysearch/SearchInRotatedSortedArray.java
@@ -4,26 +4,26 @@ public class SearchInRotatedSortedArray {
 
     public static void main(String[] args) {
         SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
-        int index = searchInRotatedSortedArray.search(new int[]{4,5,6,7,0,1,2}, 0);
-        System.out.println(index);
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        System.out.println(searchInRotatedSortedArray.search(nums, 0));
     }
 
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            }
-            if (nums[left] <= nums[mid]) {
+            } else if (nums[left] < nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && target <= nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
