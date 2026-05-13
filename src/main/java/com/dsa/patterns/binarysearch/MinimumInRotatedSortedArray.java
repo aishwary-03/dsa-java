13db44ddfd5d2d4e423f894dda8292d4bf149cef
aishwary-03 <aishwary987654321@gmail.com>
2026-05-13 15:40:04 +0530

    Minimum In Rotated Sorted Array

--- src/main/java/com/dsa/patterns/binarysearch/MinimumInRotatedSortedArray.java
+++ src/main/java/com/dsa/patterns/binarysearch/MinimumInRotatedSortedArray.java
@@ -1,25 +1,26 @@
 package com.dsa.patterns.binarysearch;
 
-public class FindMinimumInRotatedSortedArray {
+public class MinimumInRotatedSortedArray {
 
     public static void main(String[] args) {
-        FindMinimumInRotatedSortedArray findMinimumInRotatedSortedArray = new FindMinimumInRotatedSortedArray();
-        int min = findMinimumInRotatedSortedArray.findMin(new int[]{4, 5, 6, 7, 0, 1, 2});
-        System.out.println(min);
+        MinimumInRotatedSortedArray minimumInRotatedSortedArray = new MinimumInRotatedSortedArray();
+        System.out.println(minimumInRotatedSortedArray.findMin(new int[]{5,1,2,3,4}));
     }
 
     public int findMin(int[] nums) {
+
         int left = 0;
         int right = nums.length - 1;
+
         while (left < right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] > nums[right]) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-
         return nums[left];
     }
 
