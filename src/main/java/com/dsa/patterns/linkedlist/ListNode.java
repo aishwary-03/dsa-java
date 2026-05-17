6db485722d921719b687106963809e2caa7b2448
aishwary-03 <aishwary987654321@gmail.com>
2026-05-17 20:38:45 +0530

    Reverse Linked List

--- src/main/java/com/dsa/patterns/linkedlist/ListNode.java
+++ src/main/java/com/dsa/patterns/linkedlist/ListNode.java
@@ -2,17 +2,17 @@ package com.dsa.patterns.linkedlist;
 
 public class ListNode {
 
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
