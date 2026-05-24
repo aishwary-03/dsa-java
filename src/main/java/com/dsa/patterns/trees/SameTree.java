7263341bb788275021b4af4ee3e7e7ea120fd6a6
aishwary-03 <aishwary987654321@gmail.com>
2026-05-24 12:38:57 +0530

    Same Tree

---
+++ src/main/java/com/dsa/patterns/trees/SameTree.java
@@ -0,0 +1,26 @@
+package com.dsa.patterns.trees;
+
+public class SameTree {
+
+    public static void main(String[] args) {
+        SameTree sameTree = new SameTree();
+
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(1);
+
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(1);
+        q.right = new TreeNode(2);
+
+        System.out.println(sameTree.isSameTree(p, q));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
