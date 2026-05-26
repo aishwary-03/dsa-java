5220a590847dce9373d024ed91a799b7eecbbe50
aishwary-03 <aishwary987654321@gmail.com>
2026-05-26 10:26:39 +0530

    Kth Largest Element in an Array

---
+++ src/main/java/com/dsa/patterns/heaps/KthLargestElementInAnArray.java
@@ -0,0 +1,23 @@
+package com.dsa.patterns.heaps;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElementInAnArray {
+
+    public static void main(String[] args) {
+        KthLargestElementInAnArray kthLargestElementInAnArray = new KthLargestElementInAnArray();
+        System.out.println(kthLargestElementInAnArray.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+
+}
