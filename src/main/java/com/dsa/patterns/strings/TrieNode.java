33bb1692712df28c83b6e097610e0b742ef8876a
aishwary-03 <aishwary987654321@gmail.com>
2026-05-30 00:37:05 +0530

    Implement Trie

---
+++ src/main/java/com/dsa/patterns/strings/TrieNode.java
@@ -0,0 +1,12 @@
+package com.dsa.patterns.strings;
+
+public class TrieNode {
+
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndOfWord = false;
+    }
+}
