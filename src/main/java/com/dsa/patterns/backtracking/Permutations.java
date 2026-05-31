faba5dff9be5d16a2195cffca45bc1807ec1a2b1
aishwary-03 <aishwary987654321@gmail.com>
2026-05-31 18:59:23 +0530

    Permutations

---
+++ src/main/java/com/dsa/patterns/backtracking/Permutations.java
@@ -0,0 +1,37 @@
+package com.dsa.patterns.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+    public static void main(String[] args) {
+        Permutations permutations = new Permutations();
+        System.out.println(permutations.permute(new int[]{1, 2, 3}));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, List<Integer> path, boolean[] used, List<List<Integer>> result) {
+
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            path.add(nums[i]);
+            used[i] = true;
+
+            backtrack(nums, path, used, result);
+
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+}
