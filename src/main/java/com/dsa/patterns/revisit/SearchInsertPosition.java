5991820149899a87bf9c073ef3ea3a0b6afbce34
aishwary-03 <aishwary987654321@gmail.com>
2026-06-10 12:14:41 +0530

    Search Insert Position

---
+++ src/main/java/com/dsa/patterns/revisit/SearchInsertPosition.java
@@ -0,0 +1,26 @@
+package com.dsa.patterns.revisit;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+        System.out.println(searchInsertPosition.searchInsert(new int[]{1,3,5,6}, 7));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left= mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
