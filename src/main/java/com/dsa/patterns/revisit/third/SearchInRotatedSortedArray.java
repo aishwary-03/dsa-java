9a66191e11b5b891711c27fe283041f0e9364206
aishwary-03 <aishwary987654321@gmail.com>
2026-06-13 20:12:50 +0530

    Search In a Rotated Sorted Array

---
+++ src/main/java/com/dsa/patterns/revisit/third/SearchInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+package com.dsa.patterns.revisit.third;
+
+public class SearchInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 3;
+        System.out.println(searchInRotatedSortedArray.search(nums, target));
+    }
+
+    private int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
