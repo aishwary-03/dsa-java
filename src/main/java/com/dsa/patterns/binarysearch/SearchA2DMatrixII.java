fc2142f603377797a6212a8c2e855d0e653b55c4
aishwary-03 <aishwary987654321@gmail.com>
2026-06-13 14:09:50 +0530

    Search A 2D Matrix II

---
+++ src/main/java/com/dsa/patterns/binarysearch/SearchA2DMatrixII.java
@@ -0,0 +1,32 @@
+package com.dsa.patterns.binarysearch;
+
+public class SearchA2DMatrixII {
+
+    public static void main(String[] args) {
+        SearchA2DMatrixII searchA2DMatrixII = new SearchA2DMatrixII();
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        System.out.println(searchA2DMatrixII.searchMatrix(matrix, 13));
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
